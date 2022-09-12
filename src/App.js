b17baeeb72eb815b220f98a0b464a565bb2bd2b4
Alexander Hasl <alexander.hasl@adesso.de>
2022-09-12 08:35:27 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+
+import React, { useState } from 'react';
+import { Button, ButtonGroup, ChakraProvider } from '@chakra-ui/react'
+
+const BUTTONS = 6;
+
+function App() {
+  
+  const [buttons, setButton] = useState([]);
+  function changeColor(index) {
+      let newButtons = [...buttons];
+      newButtons[index] = true;
+      const count = newButtons.filter(x => x === true).length
+      if(count === BUTTONS ) {
+        setButton([false,false,false,false,false,false]);
+      } else{
+        setButton(newButtons);
+      }
+    }
+    let buttonsArr = [];
+    for(let i = 0; i < BUTTONS ; i++ ) {
+      buttonsArr.push( 
+        <Button key={i} 
+                variant="solid" 
+                colorScheme={buttons[i] ? "red": "green"} 
+                onClick={() => changeColor(i)}>Button {i}
+        </Button>);
+    }
+  return (
+    <ChakraProvider>
+      <ButtonGroup variant='outline' spacing='6'>
+       {buttonsArr}
+    </ButtonGroup>
+    </ChakraProvider>
+  )
+}
+
+export default App;
